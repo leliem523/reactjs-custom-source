26461a559629d667d96d38da43ca8544a324412d
leliem <jos.tuanliem@gmail.com>
2022-08-02 22:35:56 +0700

    fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./src/App";
 import Modules from "./src/modules";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
