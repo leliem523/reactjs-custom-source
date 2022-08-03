e877b9ea0d0fd72cda85f6b493543bd5d8efd098
leliem <jos.tuanliem@gmail.com>
2022-08-03 21:36:55 +0700

    config saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import HomeStore from '../modules/home/store'
+// src/app/store.js
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga';
+
+
+// disalbe thunk and add redux-saga middleware
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
 
 export const store = configureStore({
   reducer: {
     ...HomeStore,
   },
+  middleware
 })
+
+sagaMiddleware.run(rootSaga);
